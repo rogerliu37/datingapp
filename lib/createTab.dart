cf47c42ca001ef7445ef30b3dfa418f0bbd4d929
Tyler Liu <tylerliu888@gmail.com>
2020-07-26 00:20:24 -0400

    Updated CreateTab

--- lib/createTab.dart
+++ lib/createTab.dart
@@ -1,18 +1,168 @@
 import 'package:flutter/material.dart';
+import 'matchTabSubPage1.dart';
 
 class createTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        backgroundColor: Colors.redAccent[700],
-        body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('images/background1.png'),
-                  fit: BoxFit.cover)),
-          child: Center(
-            child: Text("create your date!"),
-          ),
+        appBar: new AppBar(
+          title: Image.asset('images/mainLogo.PNG',
+              fit: BoxFit.cover, height: 150.0),
+          backgroundColor: Colors.amber[200],
+        ),
+        backgroundColor: Colors.grey[850],
+        body: Center(
+          child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/background2.JPG'),
+                      fit: BoxFit.cover)),
+              child: createForms()),
         ));
   }
+}
+
+class createForms extends StatefulWidget {
+  createForms({Key key}) : super(key: key);
+
+  @override
+  _createFormsState createState() => _createFormsState();
+}
+
+class _createFormsState extends State<createForms> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          //First Name
+          Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.sentiment_dissatisfied),
+                  hintText: 'How ya feeling?',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          //Last Name
+          Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.sentiment_neutral),
+                  hintText: 'Neutral',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          //Email
+          Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.sentiment_satisfied),
+                  hintText: 'Email',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          //Phone Number
+          Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.phone),
+                  hintText: 'Phone number',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          //Date of Birth
+          Flexible(
+            child: Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.cake),
+                  hintText: 'Date of Birth',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          //Submit button (First Page of form)
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // Process data.
+                  //Goes to next subpage
+                  navigateToSubPage1(context);
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Future navigateToSubPage1(context) async {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => MatchTabSubPage1()));
 }
